8ff8ed0d3f830c031282f40ecaca4a735869406e
Lyubomir Filipov <lyubomir.filipov@egt-interactive.com>
2022-07-18 22:44:12 +0300

    add License information

--- sources/backend/types/BackendTeam.h
+++ sources/backend/types/BackendTeam.h
@@ -3,6 +3,23 @@
  * @brief
  * @author Lyubomir Filipov
  * @date Nov 30, 2021
+ *
+ * Copyright 2021, 2022 Lyubomir Filipov
+ *
+ * This file is part of Mattermost-QT.
+ *
+ * Mattermost-QT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattermost-QT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mattermost-QT; if not, see https://www.gnu.org/licenses/.
  */
 
 #pragma once
--- sources/channel-tree/team-item/DirectTeamItem.cpp
+++ sources/channel-tree/team-item/DirectTeamItem.cpp
@@ -3,6 +3,23 @@
  * @brief
  * @author Lyubomir Filipov
  * @date Jun 21, 2022
+ *
+ * Copyright 2021, 2022 Lyubomir Filipov
+ *
+ * This file is part of Mattermost-QT.
+ *
+ * Mattermost-QT is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Mattermost-QT is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Mattermost-QT; if not, see https://www.gnu.org/licenses/.
  */
 
 #include "DirectTeamItem.h"
