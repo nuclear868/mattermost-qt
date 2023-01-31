b6616b378ed366a3f73063df9bf914d202b14929
Lyubomir Filipov <lyubomir.filipov@egt-interactive.com>
2023-01-31 21:51:52 +0200

    Issue #5: fixed license info - GPL 3

--- sources/backend/NetworkRequest.cpp
+++ sources/backend/NetworkRequest.cpp
@@ -8,18 +8,18 @@
  *
  * This file is part of Mattermost-QT.
  *
- * Mattermost-QT is free software; you can redistribute it and/or modify
+ * Mattermost-QT is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * Mattermost-QT is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with Mattermost-QT; if not, see https://www.gnu.org/licenses/.
+ * along with Mattermost-QT. if not, see https://www.gnu.org/licenses/.
  */
 
 #include "NetworkRequest.h"
