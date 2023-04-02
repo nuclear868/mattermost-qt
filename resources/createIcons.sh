ed541398e129f5d1e1d79096dde508461741d52f
Lyubomir Filipov <lyubomir.filipov@egt-interactive.com>
2023-04-02 22:07:48 +0300

    reorganize icon files

--- resources/createIcons.sh
+++ resources/createIcons.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-../tools/svgToPng.sh "16 32 64 128 256 512 1024" icons/application-dark/mattermost-qt.svg
-../tools/svgToPng.sh "16 32 64 128 256 512 1024" icons/application-light/mattermost-qt.svg
+../tools/svgToPng.sh "16 32 64 128 256 512 1024" svg/mattermost-qt-dark.svg icons/application-dark/
+../tools/svgToPng.sh "16 32 64 128 256 512 1024" svg/mattermost-qt-light.svg icons/application-light/
--- tools/svgToPng.sh
+++ tools/svgToPng.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-if [ "$#" != "2" ]; then
-	echo "Usage: $0 <"sizes"> <source>"
+if [ "$#" != "3" ]; then
+	echo "Usage: $0 <"sizes"> <source svg> <destination dir>"
 	echo "Note: sizes should be enclosed in quotes"
-	echo "Example: $0 \"16 32 64\" input.svg"
+	echo "Example: $0 \"16 32 64\" input.svg resources/icons"
 	exit 1
 fi
 
 sizes=($1)
-src=$2
+srcSvg=$2
+dstDir=$3
 
-if [ ! -f "$src" ]; then
-	echo "source file '$src' does not exist"
+if [ ! -f "$srcSvg" ]; then
+	echo "source file '$srcSvg' does not exist"
 	exit 1;
 fi
 
-srcBase=$(basename "$src")
-dstName="$(dirname $src)/${srcBase/.svg/}"
+srcBase=$(basename "$srcSvg")
+dstName="$dstDir/${srcBase/.svg/}"
 
 for s in "${sizes[@]}"; do
-	inkscape -w "$s" -h "$s" -o "${dstName}-${s}.png" "$src"
+	inkscape -w "$s" -h "$s" -o "${dstName}-${s}.png" "$srcSvg"
 done
 
